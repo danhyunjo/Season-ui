5bcb8b39e4b0f963d020911a014576588b2dc9f4
daeun0726 <delee2008@naver.com>
2024-11-06 01:11:34 +0900

    feat : index.js 컴포넌트 export

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,7 @@
 export { default as GreenGiftBox } from './giftButton/GreenGiftBox';
 export { default as MusicBar } from './musicBar/MusicBar';
 export { default as TabList } from './tabList/TabList';
+export { default as Card } from './cardSlider/Card';
+export { default as TabList } from './tabList/TabList';
+export { default as RedGiftBox } from './giftButton/RedGiftBox';
+export { default as Loading } from './loading/Loading';
